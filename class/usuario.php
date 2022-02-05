51c279ed5cf993d94b6f92b320072b026ce389f6
gabrielsouzaleite <gabriel.s.leite05@gmail.com>
2022-02-05 16:07:05 -0300

    metodos de lista

--- class/usuario.php
+++ class/usuario.php
@@ -33,6 +33,22 @@ class usuario{
     $this->dtcadastro = $values;
   }
 
+  public static function getList(){
+    $sql = new sql();
+
+    return $sql->select("SELECT * FROM tb_usuarios");
+  }
+
+  public static function search($login){
+    $sql = new sql();
+
+    return $sql->select("SELECT * FROM tb_usuarios WHERE deslogin LIKE :SEARCH ORDER BY deslogin", array(
+      ":SEARCH"=>"%".$login."%"
+
+    ));
+
+  }
+
   public function loadById($id){
     $sql = new sql();
 
@@ -58,6 +74,27 @@ class usuario{
       "dessenha"=>$this->getDessenha(),
       "dtcadastro"=>$this->getDtcadastro()->format("d/m/Y H:i:s")
     ));
+  }
+
+  public function login($login, $password){
+    $sql = new sql();
+
+    $results = $sql->select("SELECT * FROM tb_usuarios WHERE deslogin = :LOGIN AND dessenha = :PASSWORD", array(
+      ":LOGIN"=>$login,
+      ":PASSWORD"=>$password
+    ));
+
+    if(count($results) > 0){
+      $row = $results[0];
+
+      $this->setIdusuario($row['idusuario']);
+      $this->setDeslogin($row['deslogin']);
+      $this->setDessenha($row['dessenha']);
+      $this->setDtcadastro(new DateTime($row['dtcadastro']));
+
+    } else {
+      echo "ta errado";
+    }
   }
 }
 ?>
--- index.php
+++ index.php
@@ -2,15 +2,26 @@
 
 require_once("config.php");
 
+//consulta no banco
 // $sql = new Sql();
-
 // $usuarios = $sql->select("SELECT * FROM tb_usuarios");
-
 // echo json_encode($usuarios);
 
-$user = new usuario();
+//consulta de usuário por id
+// $user = new usuario();
+// $user->loadById(2);
+// echo $user;
 
-$user->loadById(2);
+//consulta por lista
+//$list = usuario::getList();
+//echo json_encode($list);
 
-echo $user;
+//Procura usuário
+//$search = usuario::search("R");
+//echo json_encode($search);
+
+//carrega um usuário pelo login e senha
+//$usuario = new usuario();
+//$usuario->login("gabriel","0505");
+//echo $usuario;
 ?>
